af9a588ea5b33b5d1278aaa66f820f8aa2138802
SonomaruNata <119049910+SonomaruNata@users.noreply.github.com>
2023-02-06 20:18:23 +0200

    java

---
+++ 06.02/script2.js
@@ -0,0 +1,12 @@
+let greeting= "welcome";
+let myName = "Nata";
+let msg = greeting + "," + myName;
+alert (msg);
+let age = 37;
+age*=2;
+if ( age > 80) { 
+    alert("you are above 40");
+}
+else  { 
+    alert("you are less than 40");
+}
